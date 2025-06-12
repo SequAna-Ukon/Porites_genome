a27097bc51a5abb1d04d5815d8b2a085df798efa
Anna Fiesinger <123569008+afiesinger@users.noreply.github.com>
2025-06-12 09:40:50 +0200

    Update 01.kmer.sh

--- 01.kmer.sh
+++ 01.kmer.sh
@@ -1,23 +1,13 @@
 #!/bin/bash 
 
 conda activate kmer
-conda install -c bioconda jellyfish
+conda install bioconda::meryl
 
-# jellyfish count 
-jellyfish count -C -m 21 -s 600M -t 32 *.fastq -o Phar_reads.jf
+# count kmers
+meryl count k=21 output Phar_porechopped_reads.meryl Phar_porechopped_reads.fastq.gz
 
 # generate kmer histogram
-jellyfish histo -t 32 Phar_reads.jf > Phar_reads.histo
+meryl histogram Phar_porechopped_reads.meryl > Phar_porechopped_reads.meryl.hist
 
 # # GENOMESCOPE # #
-Rscript genomescope.R Phar_reads.histo 21 150 Phar_genomescope_21
-
-# # SMUDGEPLOT # # 
-L=$(smudgeplot.py cutoff Phar_03_reads.histo L)
-U=$(smudgeplot.py cutoff Phar_03_reads.histo U)
-
-echo $L $U # these need to be sane values like 30 800 or so
-
-jellyfish dump -c -L $L -U $U Phar_reads.jf | smudgeplot.py hetkmers -o kmer_pairs
-
-smudgeplot.py plot kmer_pairs_coverages.tsv -o Phar_smudgeplot
+Rscript genomescope.R Phar_porechopped_reads.meryl.hist 21 150 Phar_genomescope_21
