1a27301b5e561ef5cbe5c43b75819978f37ff9ba
Anna Fiesinger <123569008+afiesinger@users.noreply.github.com>
2025-06-12 09:48:18 +0200

    Update 01.kmer.sh

--- 01.kmer.sh
+++ 01.kmer.sh
@@ -4,10 +4,10 @@ conda activate kmer
 conda install bioconda::meryl
 
 # count kmers
-meryl count k=21 output Phar_porechopped_reads.meryl Phar_porechopped_reads.fastq.gz
+meryl count k=21 output Phar_reads_porechopped.meryl Phar_reads_porechopped.fastq.gz
 
 # generate kmer histogram
-meryl histogram Phar_porechopped_reads.meryl > Phar_porechopped_reads.meryl.hist
+meryl histogram Phar_reads_porechopped.meryl > Phar_meryldb.hist
 
 # # GENOMESCOPE # #
-Rscript genomescope.R Phar_porechopped_reads.meryl.hist 21 150 Phar_genomescope_21
+Rscript genomescope.R Phar_meryldb.hist 21 150 Phar_genomescope_21
